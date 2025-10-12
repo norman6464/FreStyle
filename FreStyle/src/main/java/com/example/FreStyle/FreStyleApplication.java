ea8e8d097376ea95f523992b782ce9048492321d
norman6464 <resjimkalto89890@emaiol.com>
2025-10-12 13:52:46 +0900

    create project

---
+++ FreStyle/src/main/java/com/example/FreStyle/FreStyleApplication.java
@@ -0,0 +1,13 @@
+package com.example.FreStyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreStyleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreStyleApplication.class, args);
+	}
+
+}
---
+++ FreStyle/src/test/java/com/example/FreStyle/FreStyleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FreStyle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreStyleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

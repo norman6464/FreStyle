b62e64f81918318a02e8c677e9aec0c111cc7d5c
norman6464 <resjimkalto89890@emaiol.com>
2025-11-11 15:27:43 +0900

    proxy server

--- FreStyle/src/main/java/com/example/FreStyle/auth/JwtCookieFilter.java
+++ FreStyle/src/main/java/com/example/FreStyle/auth/JwtCookieFilter.java
@@ -7,33 +7,43 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.io.IOException;
 
-// 1リクエスト1回のみ実行するクラスを継承をしている
+// 1リクエスト1回のみ実行するクラスを継承している
 @Component
 public class JwtCookieFilter extends OncePerRequestFilter {
 
-  @Override
-  protected void doFilterInternal(HttpServletRequest request,
-      HttpServletResponse response,
-      FilterChain filterChain)
-      throws ServletException, IOException {
+    // ← ここでLoggerを宣言
+    private static final Logger log = LoggerFactory.getLogger(JwtCookieFilter.class);
 
-    HttpServletRequest wrappedRequest = request;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
 
-    if (request.getCookies() != null) {
-      for (Cookie cookie : request.getCookies()) {
-        if ("ACCESS_TOKEN".equals(cookie.getName())) {
-          String bearerToken = "Bearer " + cookie.getValue();
-          wrappedRequest = new HttpServletRequestWrapperWithHeader(request, "Authorization", bearerToken);
-          System.out.println("JwtCookieFilter: wrapped request with Authorization header");
-          break;
+        String proto = request.getHeader("X-Forwarded-Proto");
+        String forwardedFor = request.getHeader("X-Forwarded-For");
+
+        // SLF4Jでログ出力
+        log.info("Incoming request: method={}, uri={}, X-Forwarded-Proto={}, X-Forwarded-For={}",
+                 request.getMethod(), request.getRequestURI(), proto, forwardedFor);
+
+        HttpServletRequest wrappedRequest = request;
+
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                if ("ACCESS_TOKEN".equals(cookie.getName())) {
+                    String bearerToken = "Bearer " + cookie.getValue();
+                    wrappedRequest = new HttpServletRequestWrapperWithHeader(request, "Authorization", bearerToken);
+                    log.info("JwtCookieFilter: wrapped request with Authorization header");
+                    break;
+                }
+            }
         }
-      }
+
+        filterChain.doFilter(wrappedRequest, response);
     }
-
-    filterChain.doFilter(wrappedRequest, response);
-  }
-
 }

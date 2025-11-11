7c0a18b336291b97d7104db08159c15904bd77b3
norman6464 <resjimkalto89890@emaiol.com>
2025-11-11 13:21:38 +0900

    refactoring

--- FreStyle/src/main/java/com/example/FreStyle/FreStyleApplication.java
+++ FreStyle/src/main/java/com/example/FreStyle/FreStyleApplication.java
@@ -2,6 +2,9 @@ package com.example.FreStyle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @SpringBootApplication
 public class FreStyleApplication {
@@ -9,5 +12,15 @@ public class FreStyleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FreStyleApplication.class, args);
 	}
+	
+	@Bean
+public FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter() {
+    FilterRegistrationBean<ForwardedHeaderFilter> filterRegBean = new FilterRegistrationBean<>();
+    filterRegBean.setFilter(new ForwardedHeaderFilter());
+		filterRegBean.setOrder(0);
+		filterRegBean.addUrlPatterns("/*");
+    return filterRegBean;
+}
+
 
 }

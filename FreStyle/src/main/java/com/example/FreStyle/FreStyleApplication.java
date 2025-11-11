bd80fb0cfb6af28c47388cbc31215225062dc9c4
norman6464 <resjimkalto89890@emaiol.com>
2025-11-11 15:22:17 +0900

    proxy server

--- FreStyle/src/main/java/com/example/FreStyle/FreStyleApplication.java
+++ FreStyle/src/main/java/com/example/FreStyle/FreStyleApplication.java
@@ -2,9 +2,6 @@ package com.example.FreStyle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @SpringBootApplication
 public class FreStyleApplication {
@@ -12,15 +9,4 @@ public class FreStyleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FreStyleApplication.class, args);
 	}
-	
-	@Bean
-public FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter() {
-    FilterRegistrationBean<ForwardedHeaderFilter> filterRegBean = new FilterRegistrationBean<>();
-    filterRegBean.setFilter(new ForwardedHeaderFilter());
-		filterRegBean.setOrder(0);
-		filterRegBean.addUrlPatterns("/*");
-    return filterRegBean;
-}
-
-
 }

cc4f4463d0e83917ca9dbba0f2509b8cf70324ee
norman6464 <175028044+norman6464@users.noreply.github.com>
2026-01-07 13:55:15 +0900

    ECSタスク定義の刷新を行ったので再デプロイをする

--- FreStyle/src/main/java/com/example/FreStyle/controller/AiChatController.java
+++ FreStyle/src/main/java/com/example/FreStyle/controller/AiChatController.java
@@ -3,6 +3,7 @@ package com.example.FreStyle.controller;
 import java.util.List;
 import java.util.Map;
 
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
